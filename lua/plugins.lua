a2fdd23fe88794c487bf6dc3c36d854e7ca3c2de
shanmu <shanmu1901@gmail.com>
2024-11-15 01:30:39 +0800

    feat: Add markdown-preview plugins
    Note: Make sure npm is avaliable on the system

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -76,5 +76,17 @@ require("lazy").setup({
             "echasnovski/mini.pick",         -- optional
         },
         config = true,
-    }   
+    },
+
+    --- Markdown Preview
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = "cd app && npm install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    },
 })
