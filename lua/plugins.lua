2e5d294de8544c15d160a0076c72049abe9d4dc1
shannmu <shanmu1901@gmail.com>
2024-11-16 00:00:37 +0800

    fix: Can not use `&&` in nushell

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -83,7 +83,7 @@ require("lazy").setup({
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
         ft = { "markdown" },
-        build = "cd app && npm install",
+        build = "cd app ; npm install",
         init = function()
             vim.g.mkdp_filetypes = { "markdown" }
         end,
